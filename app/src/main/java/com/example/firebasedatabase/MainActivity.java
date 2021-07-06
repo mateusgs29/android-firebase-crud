5e98bdd55211e242230d9f645d6792814e918a2f
Mateus Gomes <mateus.gs1999@gmail.com>
2021-07-06 01:57:29 -0300

    Main atualizada

--- app/src/main/java/com/example/firebasedatabase/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.firebasedatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnToCreate;
+    Button btnToList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToCreate = findViewById(R.id.btn_to_create);
+        btnToList = findViewById(R.id.btn_to_list);
+
+        btnToCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateActivity.class));
+            }
+        });
+
+        btnToList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+            }
+        });
     }
 }
