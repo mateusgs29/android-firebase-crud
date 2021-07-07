9720f7c249c8df42223faa98578deb466b2d0bdb
Mateus Gomes <mateus.gs1999@gmail.com>
2021-07-07 00:47:51 -0300

    funcoes de criacao e adicionar no firebase

--- app/src/main/java/com/example/firebasedatabase/CreateActivity.java
+++ app/src/main/java/com/example/firebasedatabase/CreateActivity.java
@@ -4,26 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class CreateActivity extends AppCompatActivity {
 
-    EditText createName;
-    EditText createCPF;
-    EditText createEmail;
-    EditText createPhone;
-    Button btnCreate;
-    Button btnCancel;
+    private EditText createName;
+    private EditText createEmail;
+    private EditText createPhone;
+    private Button btnCreate;
+    private Button btnCancel;
+
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
         createName = findViewById(R.id.create_name);
-        createCPF = findViewById(R.id.create_cpf);
         createEmail = findViewById(R.id.create_email);
         createPhone = findViewById(R.id.create_phone);
         btnCreate = findViewById(R.id.btn_create);
@@ -32,8 +41,40 @@ public class CreateActivity extends AppCompatActivity {
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CreateActivity.this, MainActivity.class));
+                try {
+                    if (TextUtils.isEmpty(createName.getText().toString())) {
+                        Toast.makeText(CreateActivity.this, "Nome não informado", Toast.LENGTH_SHORT).show();
+                    } else if (TextUtils.isEmpty(createEmail.getText().toString())) {
+                        Toast.makeText(CreateActivity.this, "E-mail não informado", Toast.LENGTH_SHORT).show();
+                    } else if (TextUtils.isEmpty(createPhone.getText().toString())) {
+                        Toast.makeText(CreateActivity.this, "Telefone não informado", Toast.LENGTH_SHORT).show();
+                    } else {
+                        People p = new People();
+                        p.setUid(UUID.randomUUID().toString());
+                        p.setName(createName.getText().toString());
+                        p.setEmail(createEmail.getText().toString());
+                        p.setPhone(createPhone.getText().toString());
+                        dbRef.child("People").child(p.getUid()).setValue(p);
+                        clearInputs();
+                        startActivity(new Intent(CreateActivity.this, ListActivity.class));
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(CreateActivity.this, "Não foi possível cadastrar", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearInputs();
+            }
+        });
+    }
+
+    private void clearInputs() {
+        createName.setText("");
+        createEmail.setText("");
+        createPhone.setText("");
     }
 }
